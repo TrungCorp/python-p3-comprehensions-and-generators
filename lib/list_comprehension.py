199b73daab1719339a01002829a52e52d409c618
TrungCorp <trungle9995@gmail.com>
2024-10-04 15:09:35 -0400

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    list_obj = [ k for k in num_list if (k%2 ==0)]
+    return list_obj
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [k +"!" for k in sentence_list ]
+    return new_list
